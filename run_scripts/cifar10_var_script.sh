afe9d002f19461aeaf51087c85f6b06d3afc86cf
Praveen Yadav <heypraveenyadav@gmail.com>
2022-01-12 13:18:37 +0100

    Ran some experiments

--- run_scripts/cifar10_var_script.sh
+++ run_scripts/cifar10_var_script.sh
@@ -1,21 +1,25 @@
 #!/usr/local_rwth/bin/zsh
-### ask for 8 GB memory
-#SBATCH --mem-per-cpu=8G   #M is the default and can therefore be omitted, but could also be K(ilo)|G(iga)|T(era)
+### ask for 10 GB memory
+#SBATCH --mem-per-cpu=6G   #M is the default and can therefore be omitted, but could also be K(ilo)|G(iga)|T(era)
 ### name the job
-#SBATCH --job-name=cifar10
+#SBATCH --job-name=cifar10_var
 ### job run time
-#SBATCH --time=40:00:00
+#SBATCH --time=30:00:00
 ### declare the merged STDOUT/STDERR file
-#SBATCH --output=cifar10_logs.%J.txt
+#SBATCH --output=cifar10_var_logs.%J.txt
+###
+#SBATCH --mail-type=ALL
+###
+#SBATCH --mail-user=praveen.yadav@rwth-aachen.de
 ### request a GPU
-#SBATCH --gres=gpu:pascal:1
+#SBATCH --gres=gpu:volta:1
 
 ### begin of executable commands
 cd $HOME/repos/CosDefence/federated_learning
 ### load modules
 module switch intel gcc
-module load python
-module load cuda/111
+module load python/3.8.7
+module load cuda/11.1
 module load cudnn/8.0.5
 # pip3 install --user tensorboard
 # pip3 install --user -U scikit-learn
@@ -23,4 +27,4 @@ module load cudnn/8.0.5
 # pip3 install -U kaleido
 # pip3 install pyyaml
 
-python3 run_config_variations.py cifar10_modified.yaml
+python3 run_config_variations_ffa_layer_csep.py cifar10_modified.yaml
